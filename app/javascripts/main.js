83e7df7dd31f1d46e447bc4c5812c30d659e35ca
Colin Rotherham <work@colinr.com>
2026-02-10 12:36:39 +0000

    Remove duplicate `initAll()`

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -1,5 +1,4 @@
-import { createAll, initAll } from '/nhsuk-frontend/nhsuk-frontend.min.js'
+import { createAll } from '/nhsuk-frontend/nhsuk-frontend.min.js'
 import { ImageMap } from './image-map.js'
 
-initAll()
 createAll(ImageMap)
