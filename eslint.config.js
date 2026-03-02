b72f8203e26fb2a21dace08dc09abaea2ba4097e
Colin Rotherham <work@colinr.com>
2026-03-02 22:27:36 +0000

    Add ESLint type aware linting

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import { includeIgnoreFile } from '@eslint/compat'
 import eslint from '@eslint/js'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import configPrettier from 'eslint-config-prettier/flat'
+import pluginESx from 'eslint-plugin-es-x'
 import pluginImport from 'eslint-plugin-import'
 import pluginJest from 'eslint-plugin-jest'
 import pluginJestDom from 'eslint-plugin-jest-dom'
@@ -30,7 +31,9 @@ export default defineConfig([
     ],
     languageOptions: {
       parserOptions: {
-        ecmaVersion: 'latest'
+        ecmaVersion: 'latest',
+        projectService: true,
+        tsconfigRootDir: rootPath
       }
     },
     rules: {
@@ -141,8 +144,9 @@ export default defineConfig([
     files: ['app/javascripts/**/*.{cjs,js,mjs}'],
     ignores: ['**/*.test.{cjs,js,mjs}'],
     extends: [
-      pluginTypeScript.configs.strict,
-      pluginTypeScript.configs.stylistic
+      pluginTypeScript.configs.strictTypeChecked,
+      pluginTypeScript.configs.stylisticTypeChecked,
+      pluginESx.configs['flat/restrict-to-es2015']
     ],
     languageOptions: {
       globals: globals.browser,
@@ -150,6 +154,74 @@ export default defineConfig([
         // Note: Allow ES2015 for import/export syntax
         ecmaVersion: 2015
       }
+    },
+    rules: {
+      // Allow void return shorthand in arrow functions
+      '@typescript-eslint/no-confusing-void-expression': [
+        'error',
+        {
+          ignoreArrowShorthand: true
+        }
+      ],
+
+      // Check type support for template string implicit `.toString()`
+      '@typescript-eslint/restrict-template-expressions': [
+        'error',
+        {
+          allowBoolean: true,
+          allowNumber: true
+        }
+      ],
+
+      // Promise.prototype.catch() errors cannot be typed in JavaScript
+      '@typescript-eslint/use-unknown-in-catch-callback-variable': 'off',
+
+      // Babel transpiles ES2020 class fields
+      'es-x/no-class-fields': 'off',
+
+      // Babel transpiles ES2022 class instance fields
+      'es-x/no-class-instance-fields': 'off',
+
+      // Babel transpiles ES2022 class static fields
+      'es-x/no-class-static-fields': 'off',
+
+      // ES modules include ES2016 '[].includes()' coverage
+      // https://browsersl.ist/#q=supports+es6-module+and+not+supports+array-includes
+      'es-x/no-array-prototype-includes': 'off',
+
+      // Babel transpiles ES2020 `??` nullish coalescing
+      'es-x/no-nullish-coalescing-operators': 'off',
+
+      // ES modules include ES2017 'Object.entries()' coverage
+      // https://browsersl.ist/#q=supports+es6-module+and+not+supports+object-entries
+      'es-x/no-object-entries': 'off',
+
+      // Babel transpiles optional catch binding
+      'es-x/no-optional-catch-binding': 'off',
+
+      // Babel transpiles ES2020 optional chaining
+      'es-x/no-optional-chaining': 'off',
+
+      // JSDoc blocks are optional but must be valid
+      'jsdoc/require-jsdoc': [
+        'error',
+        {
+          enableFixer: false,
+          require: {
+            FunctionDeclaration: false
+          }
+        }
+      ],
+
+      // JSDoc @param types are mandatory for JavaScript
+      'jsdoc/require-param-description': 'off',
+      'jsdoc/require-param-type': 'error',
+      'jsdoc/require-param': 'off',
+
+      // JSDoc @returns is optional
+      'jsdoc/require-returns-description': 'off',
+      'jsdoc/require-returns-type': 'off',
+      'jsdoc/require-returns': 'off'
     }
   },
   {
