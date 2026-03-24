e3e0b060a93b6873c6bbf609c0db8c934085ddd0
Colin Rotherham <work@colinr.com>
2026-03-24 17:03:06 +0000

    Add autoprefixer

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-import NHSPrototypeKit from 'nhsuk-prototype-kit'
+import autoprefixer from 'autoprefixer'
+import { sassPlugin } from 'esbuild-sass-plugin'
+import NHSPrototypeKit, { config } from 'nhsuk-prototype-kit'
+import postcss from 'postcss'
 
 import { sessionDataDefaults } from './app/data/session-data-defaults.js'
 import { routes } from './app/routes.js'
+
+const processor = postcss([
+  autoprefixer({
+    env: 'stylesheets'
+  })
+])
 
 const prototype = await NHSPrototypeKit.init({
   serviceName: 'Image map',
@@ -9,6 +18,22 @@ const prototype = await NHSPrototypeKit.init({
     entryPoints: [
       'app/stylesheets/application.scss',
       'app/javascripts/application.js'
+    ],
+    plugins: [
+      sassPlugin({
+        embedded: true,
+        loadPaths: config.modulePaths,
+        quietDeps: true,
+        sourceMap: true,
+        sourceMapIncludeSources: true,
+        async transform(css, resolveDir, filePath) {
+          const result = await processor.process(css, {
+            from: filePath
+          })
+
+          return result.css
+        }
+      })
     ]
   },
   viewsPath: ['app/views/'],
